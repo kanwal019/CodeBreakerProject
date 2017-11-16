8b51be9ad0a9806a98796805b855e6c7619fe313
kanwal019 <kanwal019@gmail.com>
2017-11-16 22:44:38 +0530

    First Commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,7 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    //First Commit Check
 }
 
 //implement new functions here
